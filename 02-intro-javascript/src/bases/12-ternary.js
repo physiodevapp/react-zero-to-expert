7e9661f97dad0ed42d2f14c4f4467f46c49bc0b7
ficapp2.0 <ficapp2.0@gmail.com>
2023-05-29 21:24:22 +0200

    elvis

---
+++ 02-intro-javascript/src/bases/12-ternary.js
@@ -0,0 +1,16 @@
+
+const active = true;
+
+// let message = '';
+
+// if (active) {
+//   message = 'active'
+// } else {
+//   message = 'Inactive'
+// }
+
+// const message = (active) ? 'Active' : 'Inactive';
+
+const message = active && 'Active'
+
+console.log(message)
