f6d447db901117894919906eb5f3faffb8e0aa94
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-02 21:01:42 +0200

    Hello World

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function App() {
+  return <h1>Hola mundo!!!</h1>;
+}
+
+ReactDOM.createRoot( document.getElementById('root') ).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App/>
+  </React.StrictMode>
 )
