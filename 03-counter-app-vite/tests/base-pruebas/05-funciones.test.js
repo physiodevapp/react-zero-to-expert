8b4c32af67ad3110d70a40bb1b78f2fd20b656e5
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-03 14:44:02 +0200

    toEqual & toStrictEqual

---
+++ 03-counter-app-vite/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,31 @@
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones";
+
+describe('Pruebas en 05-funciones', () => {
+  test('getUser debe retornar un objeto', () => {
+
+    const testUSer = {
+      uid: 'ABC123',
+      username: 'El_Papi1502'
+    };
+
+    const user = getUser();
+
+    expect(testUSer).toEqual(user);
+
+  });
+
+  test('getUsuarioActivo debe retornar un objeto', () => {
+
+    const name = 'Fernando';
+
+    const user = getUsuarioActivo(name);
+    
+    expect(user).toStrictEqual({
+      uid: 'ABC567',
+      username: 'Fernando'
+    })
+
+
+  })
+
+})
