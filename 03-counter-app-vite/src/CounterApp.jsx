89ae986f6a8aacd802e09ef0313c4cf4c6c07e1b
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-03 10:51:12 +0200

    events

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,17 +1,25 @@
 import PropTypes from 'prop-types'
 
-function CounterApp({value}) {
+function CounterApp({ value }) {
   // console.log(value)
+  const handleClick = () => {
+    console.log('btn clicked');
+    value = 1000;
+  }
+
   return (
     <>
       <h1>CounterApps</h1>
-      <h2>{value}</h2>
+      <h2> { value } </h2>
+      <button onClick={handleClick}>
+        +1
+      </button>
     </>
   )
 }
 
 CounterApp.propTypes = {
-  value: PropTypes.number.isRequired 
+  value: PropTypes.number.isRequired
 }
 
 CounterApp.defaultProps = {
