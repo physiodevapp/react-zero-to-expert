940f411cdb9defd48119cac512171e6785bfef28
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-04 22:29:56 +0200

    getByText & container

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -1,13 +1,27 @@
-import { render } from "@testing-library/react"
-import { FirstApp } from "../src/FirstApp"
+import { render } from "@testing-library/react";
+import { FirstApp } from "../src/FirstApp";
 
-describe('Testing <FirstApp/>>', () => { 
-  
-  test('should match with snapshot', () => { 
-    
+describe("Testing <FirstApp/>>", () => {
+  test("should match with snapshot", () => {
+    const title = "Hola, soy Goku";
+    const { container } = render(<FirstApp title={title} />);
+    // console.log(container)
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should show h1 title", () => {
+
     const title = 'Hola, soy Goku';
-    render(<FirstApp title={title}/>)
+    const { container, getByText } = render(<FirstApp title={title} />);
 
-   })
+    expect( getByText(title) ).toBeTruthy();
 
- })
+    const h1 = container.querySelector('h1');
+    // console.log(h1.innerHTML)
+
+    // expect(h1.innerHTML).toBe(title); // DOM strict equal
+    expect(h1.innerHTML).toContain(title);
+
+  });
+});
