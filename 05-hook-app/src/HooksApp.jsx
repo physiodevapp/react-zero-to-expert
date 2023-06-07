8ff55842a75338baf50277665c485bafa87a2563
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-07 21:05:10 +0200

    expose customHook

--- 05-hook-app/src/HooksApp.jsx
+++ 05-hook-app/src/HooksApp.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import CounterApp from "./01-useState/CounterApp";
+import CounterWithCustomHooks from "./01-useState/CounterWithCustomHooks";
 
 function HooksApp() {
   return (
     <>
-      <h2>HooksApp</h2>
-      <CounterApp />
+      <CounterWithCustomHooks/>
     </>
   );
 }
