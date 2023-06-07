dd5d338ed5827609f40ca446c5f54a98fe93b33c
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-07 21:46:42 +0200

    useEffect cautions

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -1,19 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Message() {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    console.log('message mounted!')
-  
+    // console.log('message mounted!')
+
+    const onMouseMove = ({ x, y }) => {
+      setCoords({ x, y });
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
+
     return () => {
-      console.log('message unmounted!')
-    }
-  }, [])
-  
+      // console.log('message unmounted!')
+
+      window.removeEventListener("mousemove", onMouseMove);
+    };
+  }, []);
 
   return (
-    <h4 className='mt-2'>User already exists</h4>
-  )
+    <>
+      <h4 className="mt-2">User already exists</h4>
+      <div>{JSON.stringify(coords)}</div>
+    </>
+  );
 }
 
-export default Message
+export default Message;
