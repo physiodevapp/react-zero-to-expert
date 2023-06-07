b9f9c92e3504f56e4e1803818c92670aaf5cf372
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-07 20:22:59 +0200

    add boostrap

---
+++ 05-hook-app/src/HooksApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HooksApp() {
+  return (
+    <h2>HooksApp</h2>
+  )
+}
+
+export default HooksApp
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import HooksApp from './HooksApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HooksApp/>
   </React.StrictMode>,
 )
