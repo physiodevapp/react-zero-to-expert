402549633fd0fa574bdd85de1a3f3382571be68a
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-14 14:27:25 +0200

    path params

--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+  const { heroId } = useParams();
+  const hero = getHeroById(heroId);
+  console.log(hero)
+  if (!hero) {
+    return <Navigate to={"/"}></Navigate>
+  }
+
   return (
-    <h1>HeroPage</h1>
-  )
-}
+    <>
+      <h1>{hero.superhero}</h1>
+    </>
+  );
+};
