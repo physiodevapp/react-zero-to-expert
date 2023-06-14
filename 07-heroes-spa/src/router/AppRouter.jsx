a8833b8e1ecefb23234083d8e70646a6aef484c6
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-14 13:07:42 +0200

    setting active class & barrel files

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-
-import { MarvelPage } from '../heroes/pages/MarvelPAge'
-import { DcPage } from '../heroes/pages/DcPage'
-import { LoginPage } from '../auth/pages/LoginPage'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { MarvelPage, DcPage } from "../heroes";
+import { LoginPage } from "../auth/";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
       <Routes>
-        <Route path='marvel' element={<MarvelPage/>}/>
-        <Route path='dc' element={<DcPage/>}/>
-        <Route path='login' element={<LoginPage/>}/>
-        <Route path='/' element={<Navigate to={'/marvel'}/>}/>
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DcPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to={"/marvel"} />} />
       </Routes>
     </>
-  )
-}
+  );
+};
