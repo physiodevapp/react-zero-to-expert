736191e79f3ad983993e8fa1185c79c0a7698a1b
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-14 15:11:30 +0200

    Search component

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import React from "react";
+
+import { HeroCard } from "../components";
+import { useForm } from "../hooks/useForm";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import queryString from "query-string";
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse(location.search);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: "",
+  });
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) return;
+
+    navigate(`?q=${searchText.toLowerCase().trim()}`);
+  };
+
   return (
-    <h1>SearchPage</h1>
-  )
-}
+    <>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
+            <input
+              type="text"
+              placeholder="Search hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
+            />
+
+            <button className="btn btn-outline-primar mt-1">Search</button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-primary">Search a hero</div>
+
+          <div className="alert alert-danger">
+            No hero with <b>{q}</b>
+          </div>
+
+          {/* <HeroCard/> */}
+        </div>
+      </div>
+    </>
+  );
+};
