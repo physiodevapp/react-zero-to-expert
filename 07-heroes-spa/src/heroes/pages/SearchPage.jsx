0cc2d8815e09830f5ed3b5b3f68bd02e4f3b299e
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-15 22:28:32 +0200

    testing query params part-2

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -37,7 +37,7 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form onSubmit={onSearchSubmit}>
+          <form onSubmit={onSearchSubmit} aria-label="form">
             <input
               type="text"
               placeholder="Search hero"
@@ -65,7 +65,7 @@ export const SearchPage = () => {
 
           <div aria-label="search-hero" className="alert alert-primary animate__animated animate__fadeIn" 
             style={{display: `${showSearch ? '' : 'none'} `}}>Search a hero</div>
-          <div className="alert alert-danger animate__animated animate__fadeIn" 
+          <div aria-label="search-no-hero" className="alert alert-danger animate__animated animate__fadeIn" 
             style={{display: `${showError ? '' : 'none'} `}}>No hero with <b>{q}</b></div>
 
           {
--- 07-heroes-spa/tests/heroes/pages/SearchPage.test.jsx
+++ 07-heroes-spa/tests/heroes/pages/SearchPage.test.jsx
@@ -1,41 +1,77 @@
-import { render, screen } from "@testing-library/react"
-import { MemoryRouter } from "react-router-dom"
-import { SearchPage } from "../../../src/heroes/pages/SearchPage"
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter, useNavigate } from "react-router-dom";
+import { SearchPage } from "../../../src/heroes/pages/SearchPage";
 
-describe('testing <SearchPage/>', () => { 
-  test('should show component correctly', () => { 
+const mockedUseNavigate = jest.fn()
+
+jest.mock("react-router-dom", () =>({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUseNavigate
+}))
+
+describe("testing <SearchPage/>", () => {
+
+  beforeAll(() => jest.clearAllMocks())
+
+  test("should show component correctly", () => {
     const { container } = render(
-      <MemoryRouter >
-        <SearchPage/>
+      <MemoryRouter>
+        <SearchPage />
       </MemoryRouter>
-    )  
+    );
     // screen.debug()
 
-    expect(container).toMatchSnapshot()
+    expect(container).toMatchSnapshot();
   });
 
-  test('should show Batman and input with query param value', () => { 
+  test("should show Batman and input with query param value", () => {
     render(
-      <MemoryRouter initialEntries={['/search?q=batman']}>
-        <SearchPage/>
+      <MemoryRouter initialEntries={["/search?q=batman"]}>
+        <SearchPage />
       </MemoryRouter>
-    )  
+    );
     // screen.debug()
-    const input = screen.getByRole('textbox')
-    const img = screen.getByRole('img')
+    const input = screen.getByRole("textbox");
+    const img = screen.getByRole("img");
 
-    expect(input.value).toBe('batman')
-    expect(img.src).toContain('/assets/heroes/dc-batman.jpg')
-  })
+    expect(input.value).toBe("batman");
+    expect(img.src).toContain("/assets/heroes/dc-batman.jpg");
+  });
 
-  test('should keep hidden Search a hero div', () => { 
+  test("should keep hidden Search a hero div", () => {
     render(
-      <MemoryRouter initialEntries={['/search?q=batman']}>
-        <SearchPage/>
+      <MemoryRouter initialEntries={["/search?q=batman"]}>
+        <SearchPage />
       </MemoryRouter>
-    )  
-    const searchHeroDiv = screen.getByLabelText('search-hero')
+    );
+    const searchHeroDiv = screen.getByLabelText("search-hero");
 
-    expect(searchHeroDiv.style.display).toBe('none')
-  })
-})
+    expect(searchHeroDiv.style.display).toBe("none");
+  });
+
+  test("should display error div if no hero found", () => {
+    render(
+      <MemoryRouter initialEntries={["/search?q=batman123"]}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+    const searchNoHeroDiv = screen.getByLabelText("search-no-hero");
+
+    expect(searchNoHeroDiv.style.display).not.toBe("none");
+  });
+
+  test("should call navigate on input change", () => {
+    render(
+      <MemoryRouter initialEntries={["/search"]}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole('form')
+
+    fireEvent.change(input, { target: { name: 'searchText', value: "batman123" } } );
+    fireEvent.submit(form)
+
+    expect(mockedUseNavigate).toHaveBeenCalledWith("?q=batman123")
+  });
+});
