00d604848c37eb4b0f914e16bbcbd4c47e0ae361
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-16 20:06:05 +0200

    AuthLayout & RegisterPage

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -2,30 +2,14 @@ import React from "react";
 import { Link as RouterLink } from 'react-router-dom'
 import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
 
 
 export const LoginPage = () => {
   return (
     <>
-      <Grid
-        container
-        spacing={0}
-        direction={"column"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
-      >
-        <Grid
-          item
-          className="box-shadow"
-          xs={3}
-          sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
-        >
-          <Typography variant="h5" sx={{ mb: 1 }}>
-            Login
-          </Typography>
-
-          <form>
+      <AuthLayout title={'Login'}>
+      <form>
             <Grid container>
               <Grid item xs={12}>
                 <TextField
@@ -64,13 +48,12 @@ export const LoginPage = () => {
                 direction={'row'}
                 justifyContent={'end'}
               >
-                <Link component={RouterLink} color='inherit' to={'/auth/register'}>Create account</Link>
+                <Link component={RouterLink} color='inherit' to={'/auth/register'}>Create an account</Link>
               </Grid>
 
             </Grid>
           </form>
-        </Grid>
-      </Grid>
+      </AuthLayout>
     </>
   );
 };
--- 08-journal-app/src/auth/pages/RegisterPage.jsx
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,62 @@
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <>
+    <AuthLayout title={'Register'}>
+    <form>
+          <Grid container>
+            <Grid item xs={12}>
+              <TextField
+                label="Name"
+                type="text"
+                placeholder="John Wick"
+                fullWidth
+              />
+            </Grid>
+            
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField
+                label="Email"
+                type="email"
+                placeholder="mail@example.org"
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Password"
+                type="password"
+                placeholder="1234abcd."
+                fullWidth
+              />
+            </Grid>
+
+            <Grid container spacing={2} sx={{ mt: 1, mb: 2 }}>
+              <Grid item xs={12}>
+                <Button variant="contained" fullWidth>
+                  Create account
+                </Button>
+              </Grid>
+            </Grid>
+
+            <Grid
+              container
+              direction={'row'}
+              justifyContent={'end'}
+            >
+              <Typography sx={{mr: 1}}> ¿Already have an account? </Typography>
+              <Link component={RouterLink} color='inherit' to={'/auth/login'}>Log in </Link>
+            </Grid>
+
+          </Grid>
+        </form>
+    </AuthLayout>
+  </>
   )
 }
