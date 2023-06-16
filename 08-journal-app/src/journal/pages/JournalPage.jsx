c4f722ee187ba5ea7e439afbda501afc188d8efd
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-16 22:27:02 +0200

    button for adding new entries

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
     <>
       <JournalLayout>
-        {/* <Typography>Tempor fugiat eiusmod occaecat mollit et ad nulla do id sit ullamco non.</Typography> */}
 
-        {/* Notes view */}
         <NoteView/>
         
-        {/* Nothing selected */}
         {/* <NothingSelectedView/> */}
         
+        <IconButton
+          size="large"
+          sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+            position: 'fixed',
+            right: 50,
+            bottom: 50
+          }}
+        >
+          <AddOutlined sx={{fontSize: 30}}/>
+        </IconButton>
 
       </JournalLayout>
     </>
