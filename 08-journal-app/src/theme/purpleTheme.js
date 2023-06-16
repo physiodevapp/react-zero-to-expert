daa06415e2abcc831e35020cb319957180d62de3
ficapp2.0 <ficapp2.0@gmail.com>
2023-06-16 19:43:46 +0200

    LoginPage styles

--- 08-journal-app/src/theme/purpleTheme.js
+++ 08-journal-app/src/theme/purpleTheme.js
@@ -4,7 +4,7 @@ import { red } from "@mui/material/colors";
 export const purpleTheme = createTheme({
   palette: {
     primary: {
-      main: '#222624'
+      main: '#262254'
     },
     secondary: {
       main: '#543884'
