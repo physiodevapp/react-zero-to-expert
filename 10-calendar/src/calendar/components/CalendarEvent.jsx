d6e63e58a1e84f0e5cbdccd61e5a8dac18661b18
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2023-06-29 22:31:02 +0200

    feat: calendar events & custom modal

---
+++ 10-calendar/src/calendar/components/CalendarEvent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const CalendarEvent = ({ event }) => {
+  const { title, user } = event;
+
+  // console.log(props)
+
+  return (
+    <>
+      <strong>{title}</strong>
+      <span> - {user.name}</span>
+    </>
+  );
+};
