6d40d7500eff2eecf6f495e07a4c586c23672247
Edu Gamboa <69968873+physiodevapp@users.noreply.github.com>
2023-07-07 12:07:55 +0200

    feat: test uiSlice

---
+++ 10-calendar/tests/store/ui/uiSlice.js
@@ -0,0 +1,21 @@
+
+import {uiSlice} from '../../../src/store/ui/uiSlice'
+
+describe('Pruebas en uiSlice', () => {
+	test('debe regresar el estado por defecto', () => {
+			
+		expect(uiSlice.getInitialState().isDateModalOpen).toBeFalsy()
+			
+	});
+
+	test('debe de cambiar el isDateModalOpen correctamente', () => {
+
+		let state = uiSlice.getInitialState()
+		state = uiSlice.reducer(state, onOpenDateModal())
+		expect(state.isDateModalOpen).toBeTruthy()
+
+		state = uiSlice.reducer(state, onCloseDateModal())
+		expect(state.isDateModalOpen).toBeFalsy()
+
+	})
+})
